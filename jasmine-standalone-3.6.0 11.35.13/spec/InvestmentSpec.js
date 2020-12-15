6c5625a53e9bfa396db2861a8136507a6700b0a7
Nicola <>
2020-12-15 10:49:01 +0100

    10

--- jasmine-standalone-3.6.0 11.35.13/spec/InvestmentSpec.js
+++ jasmine-standalone-3.6.0 11.35.13/spec/InvestmentSpec.js
@@ -1,12 +1,43 @@
 // The describe function creates a global function which equates to the "Investment" test suite
 describe("Investment", function() {
+    var stock;
+    var investment;
+
+// beforeEach is a function that executes once before each spec
+    beforeEach(function() {
+        stock = new Stock();
+        investment = new Investment({
+            stock: stock,
+            shares: 100,
+            sharePrice: 20
+        });
+    });
+        describe("when its stock share price valorizes", function() {
+            beforeEach(function(){
+                stock.sharePrice = 40;
+                });
+            it("should have a positive return of investment", function() {
+                    expect(investment.roi()).toEqual(1);
+                    });
+            it("should be a good investment", function() {
+                    expect(investment.isGood()).toEqual(true);
+                    });
+        });
 // The first acceptance criterion is described with the global function "it"
 // It contains two parameters: 1) title of the spec which is "should be of a stock", 2) a function that contains the spec code;
     it("should be of a stock", function() {
-// Assertions are written using the global Jasmine function expect along with a matcher that in this case is toBe() which indicates what comparison must be made with the values.
-        var stock = new Stock();
-        var investment = new Investment(stock);
-// We pass stock as parameter to the Investment function
+// // Assertions are written using the global Jasmine function expect along with a matcher that in this case is toBe() which indicates what comparison must be made with the values.
         expect(investment.stock).toBe(stock);
     });
+
+    it("should have the share paid price", function() {
+        expect(investment.sharePrice).toEqual(20)
+    });
+    it("should have a cost", function() {
+        expect(investment.cost).toEqual(2000);
+    });
+    it("should have the invested shares' quantity", function() {
+        expect(investment.shares).toEqual(100);
+    });
+
 });
--- jasmine-standalone-3.6.0 11.35.13/src/Investment.js
+++ jasmine-standalone-3.6.0 11.35.13/src/Investment.js
@@ -1,3 +1,12 @@
-function Investment(stock) {
-    this.stock = stock
+function Investment(params) {
+    this.stock = params.stock;
+    this.shares = params.shares;
+    this.sharePrice = params.sharePrice;
+    this.cost = this.shares * this.sharePrice;
+    Investment.prototype.roi = function() {
+        return (this.stock.sharePrice - this.sharePrice) / this.sharePrice;
+    };
+    Investment.prototype.isGood = function() {
+        return this.roi() > 0;
+    };
 };
