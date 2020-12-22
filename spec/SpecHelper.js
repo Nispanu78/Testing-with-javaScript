ae6f71c5391c2025e02775536f1465dbf2cbd111
Nicola <>
2020-12-22 09:45:34 +0100

    19

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,6 +1,8 @@
 // The addMatchers function accepts a single parameter—an object where each attribute corresponds to a new matcher. The function being defined here is not the matcher itself but a factory function to build the matcher. Its purpose, once called is to return an object containing a compare function, as follows:
 
 beforeEach(function() {
+  // changes the default timeout for demonstration purposes
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
   jasmine.addMatchers({
     toBeAGoodInvestment: function () {
